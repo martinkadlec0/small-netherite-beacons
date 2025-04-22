90e27ae054100331cdce917cb32934cbd08d618e
Martin Kadlec <harou2@gmail.com>
2025-04-22 14:07:59 +0200

    Only one netherite block needed

--- src/main/java/harou/small_netherite_beacons/mixin/BeaconBlockEntityMixin.java
+++ src/main/java/harou/small_netherite_beacons/mixin/BeaconBlockEntityMixin.java
@@ -12,29 +12,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(BeaconBlockEntity.class)
 public class BeaconBlockEntityMixin {
 
-	@Inject(at = @At("HEAD"), method = "updateLevel", cancellable = true)
+	@Inject(at = @At("RETURN"), method = "updateLevel", cancellable = true)
 	private static void updateLevel(World world, int x, int y, int z, CallbackInfoReturnable<Integer> cir) {
+		int beaconLevel = cir.getReturnValue();
+		if (beaconLevel <= 0) return;
+
 		BlockPos pos = new BlockPos(x, y, z);
-		int level = getNetheriteBaseLevel(world, pos);
-		if (level > 0) {
-			cir.setReturnValue(level);
+		if (world.getBlockState(pos.down()).isOf(Blocks.NETHERITE_BLOCK)) {
+			cir.setReturnValue(4); 
 		}
-	}
-
-	private static int getNetheriteBaseLevel(World world, BlockPos pos) {
-		if (isNetheriteBase(world, pos.down())) return 4;
-		return 0;
-	}
-
-	private static boolean isNetheriteBase(World world, BlockPos basePos) {
-		for (int x = -1; x <= 1; x++) {
-			for (int z = -1; z <= 1; z++) {
-				BlockPos pos = basePos.add(x, 0, z);
-				if (!world.getBlockState(pos).isOf(Blocks.NETHERITE_BLOCK)) {
-					return false;
-				}
-			}
-		}
-		return true;
 	}
 }
