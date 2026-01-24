530b2ca5a73a5c8392e410e6ffff29a66b542a6e
Martin Kadlec <harou2@gmail.com>
2026-01-24 13:59:16 +0100

    Migrate from Yarn to Mojang mappings

--- src/main/java/harou/small_netherite_beacons/mixin/BeaconBlockEntityMixin.java
+++ src/main/java/harou/small_netherite_beacons/mixin/BeaconBlockEntityMixin.java
@@ -1,9 +1,9 @@
 package harou.small_netherite_beacons.mixin;
 
-import net.minecraft.block.Blocks;
-import net.minecraft.block.entity.BeaconBlockEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BeaconBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,13 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(BeaconBlockEntity.class)
 public class BeaconBlockEntityMixin {
 
-	@Inject(at = @At("RETURN"), method = "updateLevel", cancellable = true)
-	private static void updateLevel(World world, int x, int y, int z, CallbackInfoReturnable<Integer> cir) {
+	@Inject(at = @At("RETURN"), method = "updateBase", cancellable = true)
+	private static void updateLevel(Level world, int x, int y, int z, CallbackInfoReturnable<Integer> cir) {
 		int beaconLevel = cir.getReturnValue();
 		if (beaconLevel <= 0) return;
 
 		BlockPos pos = new BlockPos(x, y, z);
-		if (world.getBlockState(pos.down()).isOf(Blocks.NETHERITE_BLOCK)) {
+		if (world.getBlockState(pos.below()).is(Blocks.NETHERITE_BLOCK)) {
 			cir.setReturnValue(4); 
 		}
 	}
